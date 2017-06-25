c6067067df4775fd78313f7ac4f6a4a82a7bdfea
satvirgrewal <satvir.grewal03@gmail.com>
2017-06-25 16:37:32 -0500

    Commiting

---
+++ dataStructures/src/bitManipulation/SignedNumbers.java
@@ -0,0 +1,11 @@
+package bitManipulation;
+
+public class SignedNumbers {
+
+	public static void main(String[] args){
+	int a = 0b0100000000000000000000000000001;
+	//a = ~a;
+System.out.println(a + "," + Integer.toBinaryString(a));
+}
+}
+	
